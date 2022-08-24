3049c787baeee86322b2e96e136c4c10d24e2c33
Debdut Chakraborty <debdut.chakraborty@rocket.chat>
2022-08-24 18:33:37 +0530

    new: asciiarts commands

---
+++ Commands/AsciiArts/TableflipCommand.ts
@@ -0,0 +1,24 @@
+import { IHttp, IModify, IPersistence, IRead } from '@rocket.chat/apps-engine/definition/accessors';
+import { ISlashCommand, SlashCommandContext } from '@rocket.chat/apps-engine/definition/slashcommands';
+import { sendMessageToRoom } from '../../Lib/communication';
+import type { UtilitySlashcommandsApp } from '../../UtilitySlashcommandsApp';
+import { AsciiArtCommand } from './enum';
+
+export class TableflipCommand implements ISlashCommand {
+  public command: string = 'tableflip';
+  public i18nDescription: string = 'tableflip_description';
+  public i18nParamsExample: string = 'tableflip_params';
+  public providesPreview: boolean = false;
+
+  constructor(private readonly app: UtilitySlashcommandsApp) {}
+
+  public async executor(context: SlashCommandContext, read: IRead, modify: IModify, http: IHttp, persis: IPersistence): Promise<void> {
+    const message = context.getArguments().join(' ');
+    await sendMessageToRoom({
+      modify,
+      room: context.getRoom(),
+      sender: context.getSender(),
+      text: message.length ? `${AsciiArtCommand.TABLEFLIP} ${message}` : AsciiArtCommand.TABLEFLIP,
+    });
+  }
+}
---
+++ Commands/index.ts
@@ -0,0 +1,5 @@
+import asciiarts_commands from './AsciiArts';
+
+const commands = [...asciiarts_commands] as const;
+
+export default commands;
